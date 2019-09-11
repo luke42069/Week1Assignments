9a83f577c7d75e781b74e8ae5d7802987fff3617
luke2720 <luke2720@ISS2TNPKH2.scdsb.pri>
2019-09-11 11:54:47 -0400

    finished pizzacost and momentum

---
+++ src/PizzaCost.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+public class PizzaCost {
+
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        final double labour=1.50,oven=0.75,ingr=0.5,top=0.75;
+        double toppings,tprice,size,total,base;
+        System.out.print("Enter your desired pizza diameter (in inches): ");
+        size=s.nextDouble();
+        System.out.print("Enter desired number of toppings: ");
+        toppings=s.nextDouble();
+        
+        base=size*ingr;
+        tprice=toppings*top;
+        total=base+tprice+oven+labour;
+        
+        System.out.println(String.format("Your total is $%.2f.",total));
+        
+    }
+    
+}
--- src/momentum.java
+++ src/momentum.java
@@ -1,9 +1,19 @@
 
+import java.util.Scanner;
+
+
 public class momentum {
 
 
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        double mass,vel,mom;
+        System.out.print("Enter object mass in Kg: ");
+        mass = s.nextDouble();
+        System.out.print("Enter object velocity in m/s: ");
+        vel = s.nextDouble();
+        mom=mass*vel;
+        System.out.println(String.format("Object has a momentum of %.2f Kg m/s.",mom));
     }
     
 }
